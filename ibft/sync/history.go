b2a98d919ff6b5c8277a985f0e1368b3a4210411
Alon Muroch <alonmuroch@gmail.com>
2021-05-09 09:25:53 +0300

    prev

--- ibft/sync/history.go
+++ ibft/sync/history.go
@@ -1,1 +1,30 @@
 package sync
+
+import "github.com/bloxapp/ssv/network"
+
+// HistorySync is responsible for syncing and iBFT instance when needed by
+// fetching decided messages from the network
+type HistorySync struct {
+	network network.Network
+}
+
+// NewHistorySync returns a new instance of HistorySync
+func NewHistorySync(network network.Network) *HistorySync {
+	return &HistorySync{network: network}
+}
+
+// Start the sync
+func (sync *HistorySync) Start() {
+	panic("implement HistorySync")
+}
+
+// FindHighestInstance returns the highest found instance identifier found by asking the P2P network
+func (sync *HistorySync) FindHighestInstance() []byte {
+
+	return nil
+}
+
+// FetchValidateAndSaveInstances fetches, validates and saves decided messages from the P2P network.
+func (sync *HistorySync) FetchValidateAndSaveInstances(startID []byte, endID []byte) {
+
+}
