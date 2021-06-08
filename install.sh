398a9cc4210c31b7bc0e258c137bf18aa092aa69
amir-blox <83904651+amir-blox@users.noreply.github.com>
2021-06-08 10:23:52 +0300

    Fix Install Script (#101)
    * fix yq installation

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-# install docker
+# install dependencies
 sudo apt-get -y update
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
-    yq \
     gnupg \
     lsb-release
 
@@ -13,7 +12,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# install more dependencies
+sudo apt-get -y update && \
+  DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq --no-install-recommends \
+  bash curl \
+  && rm -rf /var/lib/apt/lists/*
 
